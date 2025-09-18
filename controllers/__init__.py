f50297939aac9d998308201adaf88a47ede638da
FTP Admin <admin@ftp.local>
2025-09-18 13:52:01 +0300

    New

--- controllers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth_controller import auth_bp
-from .dashboard_controller import dashboard_bp
-from .admin_controller import admin_bp
-from .ftp_controller import ftp_bp
-from .api_controller import api_bp
-
-__all__ = ['auth_bp', 'dashboard_bp', 'admin_bp', 'ftp_bp', 'api_bp']
--- services/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth_service import AuthService
-from .ftp_service import FTPService
-from .user_service import UserService
-from .transfer_service import TransferService
-
-__all__ = ['AuthService', 'FTPService', 'UserService', 'TransferService']
